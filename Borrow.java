7940e258a9b99d5f303590ac1027ef41088105de
elahe hosseini <elahe.s.hs98@gmail.com>
2020-03-17 20:40:59 +0330

    Added base methods to classes Person and Borrow

--- Borrow.java
+++ Borrow.java
@@ -1,5 +1,24 @@
 package ElaheHosseini_HW11_Maktab33;
 
 public class Borrow {
-    
+    Date date;
+    String discName;
+
+    public Borrow(Date date, String discName) {
+        this.date = date;
+        this.discName = discName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public boolean isLate(Date deliveryDate) {
+        //todo
+        return false;
+    }
+
+    public String getDiscName() {
+        return discName;
+    }
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,22 @@
 package ElaheHosseini_HW11_Maktab33;
 
 public class Person {
+    String name;
+    int lateDays;
 
+    public Person (String name) {
+        this.name = name;
+    }
+
+    public void borrow(Disc disc, Date date) {
+
+    }
+
+    public void deliver (Disc disc, Date date){
+
+    }
+
+    public int getLateDays() {
+        return lateDays;
+    }
 }
