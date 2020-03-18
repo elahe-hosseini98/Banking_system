f95d28a66c41d192c41bb92f545018b7a8026f82
elahe hosseini <elahe.s.hs98@gmail.com>
2020-03-18 13:17:10 +0330

    Filled class Date

--- Date.java
+++ Date.java
@@ -1,4 +1,41 @@
 package ElaheHosseini_HW11_Maktab33;
 
 public class Date {
+    int day, month, year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int passedDays(Date date1, Date date2) {
+        int passedDays = 0;
+        
+        return passedDays;
+    }
 }
