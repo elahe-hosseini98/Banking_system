5263225798d47cc5d8ed2dd6fef662ececc695bb
elahe hosseini <elahe.s.hs98@gmail.com>
2020-03-19 11:39:20 +0330

    Added isEqual to Person

--- Person.java
+++ Person.java
@@ -1,5 +1,7 @@
 package ElaheHosseini_HW11_Maktab33;
 
+import java.util.Objects;
+
 public class Person {
     String name;
     int lateDays;
@@ -19,5 +21,18 @@ public class Person {
 
     public int getLateDays() {
         return lateDays;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
